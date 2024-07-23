283c1335ec2cd7234a58bcfb7058d78ce49ff9db
Frohn07 <froh1@yandex.ru>
2024-07-23 06:40:36 +0300

    Add enemy prefabs

--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
@@ -9,8 +10,36 @@ public class EnemySpawner : MonoBehaviour
 
 
 
-    private void SortEnemyList()
+    private void OnEnable()
     {
+        enemyPrefabs = enemyPrefabs.OrderBy(x => x.GetSpawmProbability()).ToList();
+        for (int i = 0; i < enemyPrefabs.Count; i++)
+        {
+            Debug.Log("enemy" + enemyPrefabs[i].GetSpawmProbability());
+        }
 
+    }
+
+
+    public Enemy GetRandomEnemy()
+    {
+        Enemy result = null;
+
+        float randChance = Random.Range(0, 101);
+
+
+        for(int i = 0; i < enemyPrefabs.Count; i++)
+        {
+            if (enemyPrefabs[i].GetSpawmProbability() <= randChance)
+            {
+                result = enemyPrefabs[i];
+                break;
+            }
+        }
+
+
+
+        return result;
+    
     }
 }
